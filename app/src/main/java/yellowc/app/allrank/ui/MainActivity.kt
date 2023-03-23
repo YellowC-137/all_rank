11d77cb16ed538a8e9f9e319190ae3c0e03c5855
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2023-03-23 11:38:59 +0900

    [FEAT] 권한 설정
    notification 권한

--- app/src/main/java/yellowc/app/allrank/ui/MainActivity.kt
+++ app/src/main/java/yellowc/app/allrank/ui/MainActivity.kt
@@ -5,11 +5,15 @@ import android.content.ComponentName
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import dagger.hilt.android.AndroidEntryPoint
 import yellowc.app.allrank.AllRankApplication
 import yellowc.app.allrank.R
@@ -47,14 +51,28 @@ class MainActivity : AppCompatActivity() {
             PackageManager.DONT_KILL_APP
         )
 
-        //getPermission()
+        getPermission()
         //initAlarm()
 
     }
 
+    private fun getPermission() {
+        TedPermission.create()
+            .setPermissionListener(object : PermissionListener {
+                //권한이 허용됐을 때
+                override fun onPermissionGranted() {}
+                //권한이 거부됐을 때
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    finish()
+                }
+            })
+            .setDeniedMessage("권한을 허용해주세요.")// 권한이 없을 때 띄워주는 Dialog Message
+            .setPermissions(android.Manifest.permission.POST_NOTIFICATIONS)// 얻으려는 권한(여러개 가능)
+            .check()
+    }
+
     private fun initAlarm() {
         val receiver = ComponentName(AllRankApplication.instance.applicationContext, BroadCastReceiver::class.java)
-
         AllRankApplication.instance.applicationContext.packageManager.setComponentEnabledSetting(
             receiver,
             PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
